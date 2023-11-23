f94e306ab32377257273ad4e1c2d44822483e813
Yury Miklashevich1 <yury_miklashevich1@epam.com>
2023-11-23 18:44:53 +0300

    added personal_project_id to user api

--- api/v1/user.py
+++ api/v1/user.py
@@ -16,4 +16,6 @@ class API(api_tools.APIBase):
 
     def get(self, **kwargs):
         user = self.module.current_user()
+        project_id = self.module.context.rpc_manager.timeout(2).projects_get_personal_project_id(user['id'])
+        user['personal_project_id'] = project_id
         return jsonify(user)
