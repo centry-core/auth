575e4171bc2b62ef9da83f6629ab315ee3d0c3b8
Aspect13 <ad13box@gmail.com>
2023-12-27 12:53:35 +0100

    token management api

--- api/v1/token.py
+++ api/v1/token.py
@@ -1,33 +1,88 @@
-from flask import g, make_response
-from flask_restful import Resource
+from datetime import datetime, timedelta
+
+from flask import jsonify, request
 from pylon.core.tools import log
 
-from tools import auth
+from tools import auth, api_tools
 
 
-class API(Resource):
-    def __init__(self, module):
-        self.module = module
+class API(api_tools.APIBase):
+    url_params = [
+        '',
+        '<string:uid>',
+    ]
 
-    # @auth.decorators.check_api(['global_view'])
-    def get(self, user_id: int = 1):
+    def get(self, uid: str | None = None, **kwargs):
+        user = self.module.current_user()
+        if not user:
+            return None, 403
+        if uid:
+            try:
+                token_data = auth.get_token(uuid=uid)
+            except RuntimeError:
+                return {'error': f'token with uid {uid} not found'}, 400
+            token_data['token'] = auth.encode_token(token_data['id'])
+            return jsonify(token_data)
+        all_tokens = auth.list_tokens(user['id'])
 
-        # user_id = g.auth.id # todo: remove
+        # log.warning('Token for user %s : %s', user, all_tokens)
+        # # {
+        # #     "expires": null,
+        # #     "id": 1,
+        # #     "name": "api",
+        # #     "user_id": 1,
+        # #     "uuid": "62b82885-6cd8-4b07-a0c2-5fc239c22ffa"
+        # # }
+        for i in all_tokens:
+            i['token'] = auth.encode_token(i['id'])
+        return jsonify(all_tokens)
 
-        all_tokens = auth.list_tokens(user_id)
-        #
-        if len(all_tokens) < 1:
-            token_id = auth.add_token(
-                user_id, "api",
-                # expires=datetime.datetime.now()+datetime.timedelta(seconds=30),
-            )
-        else:
-            token_id = all_tokens[0]["id"]
-        #
-        current_permissions = auth.resolve_permissions(
-            1, auth_data=g.auth
+    def post(self, **kwargs):
+        user = self.module.current_user()
+        if not user:
+            return None, 403
+        try:
+            name = request.json['name']
+        except KeyError:
+            return {'error': 'Name is required'}
+
+        expires = request.json.get('expires')
+        if expires:
+            allowed_measures = {'days', 'weeks', 'hours', 'minutes', 'seconds'}
+            try:
+                assert expires['measure'] in allowed_measures
+            except AssertionError:
+                return {'error': f'expires measure be in {allowed_measures}'}
+            except KeyError:
+                return {'error': f'expires must have "measure" key'}
+
+            try:
+                expire_value = int(expires['value'])
+            except ValueError:
+                return {'error': f'expires must be int, got {type(expires)}'}
+            except KeyError:
+                return {'error': f'expires must have "value" key'}
+            expires = datetime.now() + timedelta(**{expires['measure']: expire_value})
+
+        token_id = auth.add_token(
+            user_id=user['id'],
+            name=name,
+            expires=expires,
         )
-        #
-        token = auth.encode_token(token_id)
-        log.warning('Token for user %s : %s', user_id, token)
-        return make_response(token, 200)
+        token_data = auth.get_token(token_id=token_id)
+        token_data['token'] = auth.encode_token(token_id)
+        return jsonify(token_data)
+
+    def delete(self, uid: str, **kwargs):
+        user = self.module.current_user()
+        if not user:
+            return None, 403
+        try:
+            token_data = auth.get_token(uuid=uid)
+        except RuntimeError:
+            return {'error': f'token with uid {uid} not found'}, 400
+        if token_data['user_id'] != user['id']:
+            return None, 403
+
+        auth.delete_token(token_id=token_data['id'])
+        return None, 204
